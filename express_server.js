4dfc4f8f1d5610ca41ba87f4c31ad1d8f77535a1
AnthonyBianco <anthonysilvanobianco@gmail.com>
2019-10-04 00:04:22 +0000

    new changes to TinyApp

--- express_server.js
+++ express_server.js
@@ -27,18 +27,38 @@ const users = {
   }
 }
 
-// FIND USER OBJECT BY ID
+//               FIND USER OBJECT BY ID
 //========================================================
 function findUserObjectById(id) {
 let findUserByID = users[id];
 return findUserByID;
 };
 
+// NEW FUNCTION Users Can Only See Their Own Shortened URLs
+//========================================================
+function urlsForUser(id){
+  let results = {};
+  for(key in urlDatabase){
+    if (urlDatabase[key].userID === id)
+    results[key] = (urlDatabase[key]);
+  } 
+  return results;
+}
+
+// This means that the /urls page should not display URLs unless the user is logged in. It should instead display a message or prompt suggesting that they login or register first.
+
+// This also means that the /urls page will need to filter the entire list in the urlDatabase by comparing the userID with the logged-in user's ID. This filtering process should happen before the data is sent to the template for rendering.
+
+// Similarly, this also means that the /urls/:id page should display a message or prompt if the user is not logged in, or if the the URL with the matching :id does not belong to them.
+
+// Create a function named urlsForUser(id) which returns the URLs where the userID is equal to the id of the currently logged in user.
+
 //                  PROVIDES TWO URLS
 //========================================================
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
 //                  DELETE FUNCTION?
@@ -49,17 +69,20 @@ app.get("/urls/:shortURL/delete", (req, res) => {
 });
 
 //                  DELETE URL FUNCTION
-//========================================================\
+//========================================================
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
+  // MAKE SOMETHING THAT DELETES IF YOU'RE LOGGED IN BUT CANNOT DELETED OTHERWISE
+  if (req.cookies["id"] === urlDatabase[shortURL].userID) {
+    delete urlDatabase[shortURL];
+  } 
   res.redirect("/urls");
 });
 
 //               EDIT URL NAME FUNCTION
 //========================================================
 app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  urlDatabase[req.params.shortURL] = {longURL: req.body.longURL, userID: req.cookies["id"]};
   res.redirect("/urls");
 });
 
@@ -68,11 +91,6 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  // let username = user.email;
-  // before you log in: check if email and pssword correct (check this in user's object)
-  // if correct, pass cookie back, where value is the user_id
-  // res.cookie("id", user_id);
-  // res.redirect("/urls/new");
   for (key in users) {
   if  (users[key].email === email) {
     if (users[key].password === password){
@@ -120,13 +138,12 @@ app.post("/register", (req, res) => {
       password: req.body.password
     }
 
-    console.log(req.body.email);
-    console.log(newUser);
+   
    for (key in users){
     if (req.body.email.length === 0 || req.body.password.length === 0){
       res.status(400).send("Error: Email or password not entered");
     } else if (users[key].email === newUser.email) {
-      console.log(key.email, users.email);
+
       res.status(400).send("Error: User email already exists");
     } 
   }
@@ -144,12 +161,15 @@ app.get("/register", (req, res) => {
   res.render("registration", templateVars);
 });
 
+
+
 //         GENERATES RANDOM SHORT STRING FUNCTION FOR URL
 //===========================================================
 app.post("/urls", (req, res) => {
   // Log the POST request body to the console
+  console.log(req.cookies);
   shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.cookies["id"]};
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -160,6 +180,10 @@ app.get("/urls/new", (req, res) => {
     user_id: findUserObjectById(req.cookies["id"]),
     users: users 
   };
+  if (req.cookies["id"] === undefined)
+  {
+    res.redirect("/loginFile");
+  }
   res.render("urls_new", templateVars);
 });
 
@@ -177,15 +201,18 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     user_id: findUserObjectById(req.cookies["id"]),
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
   };
+  console.log(urlDatabase);
   res.render("urls_show", templateVars);
 });
 
 //               GENERATES LONG URL FUNCTION
 //============================================================
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  console.log(req.params);
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  console.log(longURL);
   res.redirect(longURL);
 });
 
@@ -193,8 +220,12 @@ app.get("/u/:shortURL", (req, res) => {
 // ===========================================================
 
 app.get("/urls", (req, res) => {
-  //console.log(req.cookies);
-  let templateVars = { urls: urlDatabase, user_id: findUserObjectById(req.cookies["id"]) };
+  let userID = req.cookies["id"];
+  if (userID === undefined){
+    //res.status(400).send("Error: Something went wrong");
+  }
+  let userCreatedUrls = urlsForUser(userID);
+  let templateVars = { urls: userCreatedUrls, user_id: findUserObjectById(req.cookies["id"])};
   res.render("urls_index", templateVars);
 });
 
